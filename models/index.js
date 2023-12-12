31a7e15cbaf94257f0a817323785f2a929ab963b
Colin Liu <colin173@gmail.com>
2023-12-12 23:31:28 +0800

    finish creating models

--- models/index.js
+++ models/index.js
@@ -3,17 +3,14 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 Product.belongsTo(Category,{
   foreignKey: 'category_id',
 })
 
-// Categories have many Products
 Category.hasMany (Product,{
   foreignKey: 'category_id'
 })
 
-// Products belongToMany Tags (through ProductTag)
 Product.belongToMany(Tag, {
   through: {
     model: ProductTag,
@@ -21,7 +18,6 @@ Product.belongToMany(Tag, {
   }
 })
 
-// Tags belongToMany Products (through ProductTag)
 Tag.belongToMany(Product, {
   through: {
     model: ProductTag,
